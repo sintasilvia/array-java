2468678781d6b2cba1207c85e8bf4643415eea42
sinta silvia <silviasinta87@gmail.com>
2021-03-09 14:10:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+    
+    static String roomArray[][] = {
+        {"*", "*", "*", "*", "x"},
+        {"*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*"},
+        {"*", "*", "*", "x", "*"},
+    };
+
+    public static void main(String[] args) {
+        int availableRoom = 0;
+        for(int i = 0; i < roomArray.length; i++) {
+            for(int j = 0; j < roomArray[i].length; j++) {
+                if(roomArray[i][j] == "*") {
+                    availableRoom++;
+                } else {
+                    System.out.println("Tamu berada di Lantai " + (i + 1)  + " Kamar " + (j + 1));
+                }
+            }
+        }
+        System.out.println("Jumlah kamar yang tersedia adalah "+ availableRoom +" kamar");
+    }
+}
